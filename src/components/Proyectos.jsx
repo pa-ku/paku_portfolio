c2d25bf40d10c7616fd4d4bd73d5542054afa103
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-12-24 17:05:54 -0300

    added more gap in proyects

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -12,7 +12,7 @@ export default function Proyectos() {
         <>
             <article className="w-full flex items-center justify-center flex-col">
                 <h2 className="w-full text-center md:text-start">{spanish ? 'Proyectos' : 'Proyects'}</h2>
-                <div className="flex gap-5 flex-wrap justify-center pt-5">
+                <div className="flex gap-10 flex-wrap justify-center pt-5">
                     {PROYECT_CARDS.map((proyect) => (
                         <Card key={proyect.name} proyect={proyect} />
                     ))}
